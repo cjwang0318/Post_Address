7a12f414ce2c43c046778fdef637f97eef8d2a47
cjwang <cjwang0318@gmail.com>
2023-07-28 20:50:07 +0800

    modify mulit_thread

--- convert_mergedpdf_to_jpg_mulit_thread.py
+++ convert_mergedpdf_to_jpg_mulit_thread.py
@@ -4,7 +4,7 @@ from pdf2image import convert_from_path
 import fitz
 from tqdm import tqdm
 import json
-import time
+import datetime
 import threading
 import queue
 import sys
@@ -28,10 +28,10 @@ def load_dict(dict_path):
     return address_dict, account_list
 
 
-def page_search(filename, search_term):
+def page_search(pdf_document, search_term):
     # filename = "example.pdf"
     # search_term = "invoice"
-    pdf_document = fitz.open(filename)
+    #pdf_document = fitz.open(filename)
     for current_page in range(len(pdf_document)):
         page = pdf_document.load_page(current_page)
         result = None
@@ -43,11 +43,11 @@ def page_search(filename, search_term):
 
 # Worker 類別，負責處理資料
 class Worker(threading.Thread):
-  def __init__(self, queue, thread_ID, pdf_file, dict):
+  def __init__(self, queue, thread_ID, pdf_document, dict):
     threading.Thread.__init__(self)
     self.queue = queue
     self.thread_ID = thread_ID
-    self.pdf_file = pdf_file
+    self.pdf_file = pdf_document
     self.dict = dict
 
   def run(self):
@@ -55,9 +55,9 @@ class Worker(threading.Thread):
       # 取得新的資料
       account_ID = self.queue.get()
       # 處理資料
-      pageID = page_search(pdf_file, account_ID)
+      pageID = page_search(self.pdf_file, account_ID)
       self.dict[account_ID]=pageID
-      print("Worker %d: %s->pageNum=%s" % (self.thread_ID, account_ID, pageID))
+      print("Worker %d: %s->pageNum=%s, queueSize= %d" % (self.thread_ID, account_ID, pageID, self.queue.qsize()))
 
 
 if __name__ == '__main__':
@@ -65,11 +65,14 @@ if __name__ == '__main__':
     #設定參數
     build_address_index = True # 是否要建立頁碼索引檔，如果不要就會讀取上次已經建立好的 ./data/pdf_merge/address_index.json
     fileName = "112.07.pdf" # 管理費帳單檔案
-    address_dict, account_list = load_dict("./data/放翁帳戶地址.txt") #放翁地址帳號索引
+    address_dict, account_list = load_dict("./data/放翁帳戶地址.csv") #放翁地址帳號索引
     #################################################################
     pdf_file = "./data/pdf_merge/" + fileName
     dict = {}
     not_convert_list = []
+    pdf_document = fitz.open(pdf_file)
+    # 開始測量
+    start = datetime.datetime.now()
 
     # Get page number
     if build_address_index:
@@ -80,13 +83,12 @@ if __name__ == '__main__':
         for account_ID in tqdm(account_list):
             my_queue.put(account_ID)
         # 建立兩個 Worker
-        my_worker1 = Worker(my_queue, 1, pdf_file, dict)
-        my_worker2 = Worker(my_queue, 2, pdf_file, dict)
-        my_worker3 = Worker(my_queue, 3, pdf_file, dict)
-        my_worker4 = Worker(my_queue, 4, pdf_file, dict)
-        my_worker5 = Worker(my_queue, 5, pdf_file, dict)
-        my_worker6 = Worker(my_queue, 6, pdf_file, dict)
-
+        my_worker1 = Worker(my_queue, 1, pdf_document, dict)
+        my_worker2 = Worker(my_queue, 2, pdf_document, dict)
+        my_worker3 = Worker(my_queue, 3, pdf_document, dict)
+        my_worker4 = Worker(my_queue, 4, pdf_document, dict)
+        my_worker5 = Worker(my_queue, 5, pdf_document, dict)
+        my_worker6 = Worker(my_queue, 6, pdf_document, dict)
         # 讓 Worker 開始處理資料
         my_worker1.start()
         my_worker2.start()
@@ -94,7 +96,6 @@ if __name__ == '__main__':
         my_worker4.start()
         my_worker5.start()
         my_worker6.start()
-
         # 等待所有 Worker 結束
         my_worker1.join()
         my_worker2.join()
@@ -102,31 +103,34 @@ if __name__ == '__main__':
         my_worker4.join()
         my_worker5.join()
         my_worker6.join()
-        print(dict)
-        sys.exit()
-
-            # pageID = page_search(pdf_file, account_ID)
-            #
-            # address = address_dict.get(account_ID)
-            # if pageID == None:
-            #     not_convert_list.append(str(account_ID) + "\n")
-            # else:
-            #     dict[pageID] = address
+        #print(dict)
+        dict_pageID_address={}
+        for account_ID, pageID in dict.items():
+            #print(account_ID, pageID)
+            if pageID == None:
+                not_convert_list.append(str(account_ID) + "\n")
+            else:
+                address = address_dict.get(account_ID)
+                dict_pageID_address[pageID] = address
         print("Started writing dictionary to a file")
         with open("./data/pdf_merge/address_index.json", "w", encoding="utf8") as fp:
-            json.dump(dict, fp, ensure_ascii=False)  # encode dict into JSON
+            json.dump(dict_pageID_address, fp, ensure_ascii=False)  # encode dict into JSON
         print("Done writing dict into json file")
         # print(dict)
     else:
         with open("./data/pdf_merge/address_index.json", "r", encoding="utf8") as fp:
             # Load the dictionary from the file
             dict = json.load(fp)
+    # 結束測量
+    end = datetime.datetime.now()
+    # 輸出結果
+    print("頁面索引執行時間：", end - start)
 
     # convert pdf to jpg
     print("Started Converting")
     if os.path.exists(pdf_file):
-        #pages = convert_from_path(pdf_file, 300, thread_count=4, poppler_path=r'D:\poppler-23.07.0\Library\bin')
-        pages = convert_from_path(pdf_file, 300, thread_count=4)
+        #pages = convert_from_path(pdf_file, 300, thread_count=8, poppler_path=r'D:\poppler-23.07.0\Library\bin')
+        pages = convert_from_path(pdf_file, 300, thread_count=8)
         for page in tqdm(pages):
             # print(pages.index(page))
             if build_address_index:
