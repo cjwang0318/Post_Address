f9f06c3326c3b3bc966670c2f2cb771fe2df2ace
Chieh-Jen Wang <cjwang0318@gmail.com>
2023-07-28 10:46:23 +0800

    version 1.0 mergedpdf_to_jpg

--- convert_mergedpdf_to_jpg.py
+++ convert_mergedpdf_to_jpg.py
@@ -39,10 +39,13 @@ def page_search(filename, search_term):
 
 
 if __name__ == '__main__':
-    build_address_index = False
-    fileName = "112.07_sample"
-    address_dict, account_list = load_dict("./data/放翁帳戶地址.txt")
-    pdf_file = "./data/pdf_merge/" + fileName + ".pdf"
+    #################################################################
+    #設定參數
+    build_address_index = False # 是否要建立頁碼索引檔，如果不要就會讀取上次已經建立好的 ./data/pdf_merge/address_index.json
+    fileName = "112.07.pdf" # 管理費帳單檔案
+    address_dict, account_list = load_dict("./data/放翁帳戶地址.csv") #放翁地址帳號索引
+    #################################################################
+    pdf_file = "./data/pdf_merge/" + fileName
     dict = {}
     not_convert_list = []
 
@@ -69,7 +72,8 @@ if __name__ == '__main__':
     # convert pdf to jpg
     print("Started Converting")
     if os.path.exists(pdf_file):
-        pages = convert_from_path(pdf_file, 300, thread_count=4, poppler_path=r'D:\poppler-23.07.0\Library\bin')
+        #pages = convert_from_path(pdf_file, 300, thread_count=4, poppler_path=r'D:\poppler-23.07.0\Library\bin')
+        pages = convert_from_path(pdf_file, 300, thread_count=4)
         for page in tqdm(pages):
             # print(pages.index(page))
             if build_address_index:
